47851c6ddcd3dea7e15c3471e3f32884a217cd01
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-11-08 07:47:11 +0200

    Customize The Checkbox Field Styling

--- src/components/forms/CheckBoxField.tsx
+++ src/components/forms/CheckBoxField.tsx
@@ -5,9 +5,31 @@ interface CheckBoxFieldProps {
 }
 const CheckBoxField: FC<CheckBoxFieldProps> = ({ label }) => {
   return (
-    <div className="flex gap-4 text-white">
-      <input type="checkbox" />
-      <label>{label}</label>
+    <div className="flex text-white items-center">
+      <input
+        id="checkbox-input"
+        type="checkbox"
+        className="appearance-none peer shrink-0 w-4 h-4 border-2 border-accent rounded-sm bg-white 
+        checked:bg-accent checked:border-0"
+      />
+      <label htmlFor="checkbox-input" className="cursor-pointer ml-2 ">
+        {label}
+      </label>
+      <svg
+        className="
+      absolute 
+      w-4 h-4 
+      hidden peer-checked:block"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <polyline points="20 6 9 17 4 12"></polyline>
+      </svg>
     </div>
   );
 };
