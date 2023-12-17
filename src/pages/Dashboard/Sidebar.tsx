d056d5fde4584a2532c06252350aa1b142726c83
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-12-17 20:23:58 +0200

    Add Drawer to sidebar in dashboard

--- src/pages/Dashboard/Sidebar.tsx
+++ src/pages/Dashboard/Sidebar.tsx
@@ -1,5 +1,7 @@
+import { Box, Drawer } from '@mui/material';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { IoMenu } from 'react-icons/io5';
 
 const pages = [
   { name: 'Editor', path: '/dashboard/editor' },
@@ -10,28 +12,68 @@ const pages = [
 
 const Sidebar = () => {
   const [selectedPage, setSelectedPage] = useState<string>('Workflows');
+  const [openSidebarDrawer, setOpenSidebarDrawer] = useState(false);
   return (
-    <div className="p-5 w-[300px] bg-secondary text-white">
-      <h3>Catalyst</h3>
-      <ul className="p-4">
-        {pages.map(page => (
-          <li
-            key={page.name}
-            className={`p-2 my-3 rounded-lg ${
-              selectedPage === page.name ? 'bg-gray-600' : 'hover:bg-gray-700'
-            }`}
-          >
-            <Link
-              to={page.path}
-              className="cursor-pointer w-full block"
-              onClick={() => setSelectedPage(page.name)}
+    <>
+      <div className="hidden lg:block p-5 w-[300px] bg-secondary text-white">
+        <h3>Catalyst</h3>
+        <ul className="p-4">
+          {pages.map(page => (
+            <li
+              key={page.name}
+              className={`p-2 my-3 rounded-lg ${
+                selectedPage === page.name ? 'bg-gray-600' : 'hover:bg-gray-700'
+              }`}
             >
-              {page.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+              <Link
+                to={page.path}
+                className="cursor-pointer w-full block"
+                onClick={() => setSelectedPage(page.name)}
+              >
+                {page.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="lg:hidden bg-primary">
+        <button
+          onClick={() => setOpenSidebarDrawer(true)}
+          className="text-white text-3xl p-2 m-3 border border-solid border-tertiary bg-secondary rounded-lg"
+        >
+          <IoMenu />
+        </button>
+      </div>
+      <Drawer
+        className="[&>.MuiDrawer-paper]:p-5 [&>.MuiDrawer-paper]:bg-secondary [&>.MuiDrawer-paper]:w-[300px] text-white"
+        open={openSidebarDrawer}
+        component={Box}
+        onClose={() => setOpenSidebarDrawer(false)}
+      >
+        <h3>Catalyst</h3>
+        <ul className="p-4">
+          {pages.map(page => (
+            <li
+              key={page.name}
+              className={`p-2 my-3 rounded-lg text-white ${
+                selectedPage === page.name ? 'bg-gray-600' : 'hover:bg-gray-700'
+              }`}
+            >
+              <Link
+                to={page.path}
+                className="cursor-pointer w-full block"
+                onClick={() => {
+                  setSelectedPage(page.name);
+                  setOpenSidebarDrawer(false);
+                }}
+              >
+                {page.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Drawer>
+    </>
   );
 };
 
