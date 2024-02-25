732344cfbc9e2a5811be15cebbb09ead0f8f6671
AmrMustafa13 <amrmostafa1311@gmail.com>
2024-02-25 22:38:43 +0200

    integrate create workflow

--- src/pages/AddWorkflow/index.tsx
+++ src/pages/AddWorkflow/index.tsx
@@ -1,13 +1,41 @@
 import Editor from '@monaco-editor/react';
 import Spinner from '@/components/Spinner';
+import { useState } from 'react';
+import { CreateWorkflowHandler } from '@/services/WorkflowApi';
+import { useNavigate } from 'react-router-dom';
+
+export interface IWorflowFormInput {
+  name: string;
+  content: string;
+}
 
 const AddWorkFlow = () => {
-  function handleEditorChange(value: string) {
-    console.log('here is the current model value:', value);
-  }
+  const navigate = useNavigate();
+
+  const [workflowName, setWorkflowName] = useState('');
+  const [workflowContent, setWorkflowContent] = useState('');
+
+  const handleCreateWorkflow: React.FormEventHandler<
+    HTMLFormElement
+  > = async event => {
+    event.preventDefault();
+    const response = await CreateWorkflowHandler({
+      name: workflowName,
+      content: workflowContent,
+    });
+    if (response.status === 'success') {
+      navigate('/dashboard/workflows');
+    } else {
+      console.log(response.error);
+    }
+  };
+
   return (
     <div className=" bg-primary">
-      <div className="container flex gap-8 items-center justify-between flex-col py-8 sm:flex-row min-h-screen">
+      <form
+        className="container flex gap-8 items-center justify-between flex-col py-8 sm:flex-row min-h-screen"
+        onSubmit={handleCreateWorkflow}
+      >
         <div className="sm:w-1/2 w-full">
           <Editor
             defaultLanguage="yaml"
@@ -15,8 +43,11 @@ const AddWorkFlow = () => {
             theme="vs-dark"
             height="100vh"
             width="100%"
-            onChange={handleEditorChange}
             loading={<Spinner />}
+            onMount={editor => {
+              editor.focus();
+            }}
+            onChange={value => setWorkflowContent(value!)}
           />
         </div>
         <div className="flex flex-col gap-4  sm:w-1/2 w-full">
@@ -26,12 +57,14 @@ const AddWorkFlow = () => {
             id="workflow-name"
             placeholder="Workflow name..."
             className=" bg-transparent  text-white p-2 border-b-2 border-accent outline-none focus:border-primary transition-all duration-300 ease-in-out w-full "
+            value={workflowName}
+            onChange={e => setWorkflowName(e.target.value)}
           />
           <button className="bg-white text-primary p-2 rounded-md hover:bg-primary hover:text-white  transition-all duration-300 ease-in-out hover:border-accent border-2 border-primary w-full whitespace-nowrap ">
             Create Workflow
           </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
---
+++ src/services/WorkflowApi.ts
@@ -0,0 +1,45 @@
+import { AxiosError } from 'axios';
+import { IWorflowFormInput } from '@/pages/AddWorkflow';
+import axiosInstance, { Response } from './axiosInstance';
+
+interface ICreateRequestBody {
+  name: string;
+  content: string;
+}
+
+interface ICreateResponseBody {
+  message: string;
+  workflow: {
+    content: string;
+    created_at: string;
+    deleted_at: string;
+    id: number;
+    name: string;
+    user_id: number;
+  };
+}
+
+export const CreateWorkflowHandler = async (
+  data: IWorflowFormInput,
+): Promise<Response<ICreateResponseBody>> => {
+  try {
+    const requestBody: ICreateRequestBody = {
+      name: data.name,
+      content: data.content,
+    };
+    const response = await axiosInstance.post('/workflows', requestBody, {
+      headers: {
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjF9.s4vE0w6cUg68FMf7GjCRpweMCQ92MdFjYM5apky7MHE`,
+      },
+    });
+    return {
+      status: 'success',
+      data: response.data,
+    };
+  } catch (errors) {
+    return {
+      status: 'error',
+      error: (errors as AxiosError).response?.data,
+    };
+  }
+};
