781ce724a185c0c9ffd0254de233148c1fa3688e
AmrMustafa13 <amrmostafa1311@gmail.com>
2024-03-15 16:32:12 +0200

    restyle landing page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,28 +10,28 @@ const Footer = () => {
     <footer className="text-white py-8">
       <div className="container flex flex-col gap-8 justify-center items-center md:flex-row md:justify-between">
         <div className="text-center">
-          All Rights Reserved &copy; Catalyst 2023
+          All Rights Reserved &copy;Catalyst 2024
         </div>
         <div>
-          <ul className="flex justify-between items-center gap-4 text-xl">
+          <ul className="flex justify-between items-center gap-4 text-2xl">
             <li>
               <a href="">
-                <AiOutlineTwitter />
+                <AiOutlineTwitter className="hover:text-accent transition-all" />
               </a>
             </li>
             <li>
               <a href="">
-                <FaFacebookF />
+                <FaFacebookF className="hover:text-accent transition-all" />
               </a>
             </li>
             <li>
               <a href="">
-                <AiOutlineInstagram />
+                <AiOutlineInstagram className="hover:text-accent transition-all" />
               </a>
             </li>
             <li>
               <a href="">
-                <AiOutlineYoutube />
+                <AiOutlineYoutube className="hover:text-accent transition-all" />
               </a>
             </li>
           </ul>
