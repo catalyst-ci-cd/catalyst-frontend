1411268668330f1cf17b5586f6ad118098172179
Khaled Hegazy <khaledhegazy870@gmail.com>
2024-03-22 22:54:23 +0200

    refactor login with google to newer non deprecated packages

--- src/components/forms/GoogleLogin.tsx
+++ src/components/forms/GoogleLogin.tsx
@@ -1,23 +1,24 @@
-import { default as ReactGoogleLogin } from 'react-google-login';
+import { useGoogleLogin } from '@react-oauth/google';
 import { AiOutlineGoogle } from 'react-icons/ai';
 
 const GoogleLogin = () => {
+  const googleLoginHook = useGoogleLogin({
+    onSuccess: ({ access_token }) => {
+      console.log(access_token);
+    },
+    onError: error => {
+      console.log(error);
+    },
+  });
   return (
-    <ReactGoogleLogin
-      clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
-      render={({ onClick }) => (
-        <button
-          type="button"
-          className="flex items-center justify-center gap-3 w-full font-medium border-2 border-solid border-accent p-3 my-3 text-accent rounded-full transition-all duration-200 hover:bg-accent hover:shadow-lg hover:text-white"
-          onClick={onClick}
-        >
-          <AiOutlineGoogle className="text-2xl" />
-          <span>Log in with Google</span>
-        </button>
-      )}
-      onSuccess={() => {}}
-      onFailure={() => {}}
-    />
+    <button
+      type="button"
+      className="flex items-center justify-center gap-3 w-full font-medium border-2 border-solid border-accent p-3 my-3 text-accent rounded-full transition-all duration-200 hover:bg-accent hover:shadow-lg hover:text-white"
+      onClick={() => googleLoginHook()}
+    >
+      <AiOutlineGoogle className="text-2xl" />
+      <span>Log in with Google</span>
+    </button>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AuthContextProvider } from './contexts/AuthContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthContextProvider>
-        <App />
-      </AuthContextProvider>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <BrowserRouter>
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 );
